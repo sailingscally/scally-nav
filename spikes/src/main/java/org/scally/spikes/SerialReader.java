4d594e5783eac18fb0016a2e2c34032538e1b3f0
Luis Martins <luis.martins@hitachivantara.com>
2020-04-22 23:53:11 +0100

    x

--- spikes/src/main/java/org/scally/spikes/SerialReader.java
+++ spikes/src/main/java/org/scally/spikes/SerialReader.java
@@ -7,21 +7,31 @@ import java.io.IOException;
 
 public class SerialReader implements SerialDataEventListener {
 
+  private static final String LINE_END = "\r\n";
+
   private StringBuffer buffer = new StringBuffer();
 
   @Override
   public void dataReceived( SerialDataEvent event) {
     try {
-      System.out.println( "RX: " + event.getAsciiString() );
+      String data = event.getAsciiString();
+      int index = data.indexOf( LINE_END );
 
-      buffer.append( event.getAsciiString() );
+      if( index != -1 ) {
+        buffer.append( data.substring( 0, index ) );
+        processLine( buffer.toString() );
 
-      if( buffer.toString().endsWith( "\n" ) ) {
-        System.out.println( "Line: " + buffer.toString() );
         buffer.delete( 0, buffer.length() );
+        buffer.append( data.substring( index ) );
+      } else {
+        buffer.append( data );
       }
     } catch ( IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private void processLine( String line ) {
+    System.out.println( "Line: " + line );
   }
 }
