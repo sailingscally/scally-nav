95e89a05c93f1b60dda5e779d774a3a0277cec78
Luis Martins <luis.martins@hitachivantara.com>
2020-04-23 00:05:30 +0100

    x

--- spikes/src/main/java/org/scally/spikes/SerialReader.java
+++ spikes/src/main/java/org/scally/spikes/SerialReader.java
@@ -7,24 +7,26 @@ import java.io.IOException;
 
 public class SerialReader implements SerialDataEventListener {
 
-  private static final String LINE_END = "\r\n";
+  private static final char CR = '\r';
+  private static final char LF = '\n';
 
   private StringBuffer buffer = new StringBuffer();
 
   @Override
   public void dataReceived( SerialDataEvent event) {
     try {
-      String data = event.getAsciiString();
-      int index = data.indexOf( LINE_END );
+      byte[] data = event.getBytes();
 
-      if( index != -1 ) {
-        buffer.append( data.substring( 0, index ) );
-        processLine( buffer.toString() );
-
-        buffer.delete( 0, buffer.length() );
-        buffer.append( data.substring( index ) );
-      } else {
-        buffer.append( data );
+      for( int i = 0; i < data.length; i ++ ) {
+        if( data[i] == CR ) {
+          // we have a whole line in the buffer, let's process it
+          processLine( buffer.toString() );
+          buffer.delete( 0, buffer.length() );
+        } else if( data[i] == LF ) {
+          // just ignore the line feed
+        } else {
+          buffer.append( data[i] );
+        }
       }
     } catch ( IOException e) {
       e.printStackTrace();
