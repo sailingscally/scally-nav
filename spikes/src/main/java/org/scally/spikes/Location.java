e03c8e071f19300a6e524d7ef8aed5f89cbb52eb
Luis Martins <luis.martins@hitachivantara.com>
2020-04-23 18:50:37 +0100

    x

--- spikes/src/main/java/org/scally/spikes/Location.java
+++ spikes/src/main/java/org/scally/spikes/Location.java
@@ -16,10 +16,14 @@ public class Location {
     while( true ) {
       Thread.sleep( 1000 );
 
-      System.out.format("UTC: %s | %s | %.8f, %.8f\n", gps.getTimeInUTC(), gps.getFixStatus(), gps.getPosition().getLatitude(), gps.getPosition().getLongitude() );
-      System.out.format( "SOG: %.1f | COG: %.1f | V: %.1f | Satellites in View: %d | PRNs: %d\n",
-        gps.getSpeedOverGround(), gps.getCourseOverGround(), gps.getVariation(), gps.getSatellitesInView().size(), gps.getPRNs().size() );
-      System.out.println();
+      try {
+        System.out.format("UTC: %s | %s | %.8f, %.8f\n", gps.getTimeInUTC(), gps.getFixStatus(), gps.getPosition().getLatitude(), gps.getPosition().getLongitude() );
+        System.out.format( "SOG: %.1f | COG: %.1f | V: %.1f | Satellites in View: %d | PRNs: %d\n",
+          gps.getSpeedOverGround(), gps.getCourseOverGround(), gps.getVariation(), gps.getSatellitesInView().size(), gps.getPRNs().size() );
+        System.out.println();
+      } catch ( Exception e ) {
+        e.printStackTrace();
+      }
     }
   }
 }
