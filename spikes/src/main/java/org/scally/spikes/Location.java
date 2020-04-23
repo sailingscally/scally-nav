33a5b0fa03a926353525340e8559808e19fbdc7c
Luis Martins <luis.martins@hitachivantara.com>
2020-04-23 18:46:02 +0100

    x

--- spikes/src/main/java/org/scally/spikes/Location.java
+++ spikes/src/main/java/org/scally/spikes/Location.java
@@ -14,12 +14,11 @@ public class Location {
     GpsAntenna gps = new GpsAntenna();
 
     while( true ) {
-      Thread.sleep( 5000 );
-
-      System.in.read();
+      Thread.sleep( 1000 );
 
       System.out.format("UTC: %s | %s | %.8f, %.8f\n", gps.getTimeInUTC(), gps.getFixStatus(), gps.getPosition().getLatitude(), gps.getPosition().getLongitude() );
-      System.out.format( "SOG: %.1f | COG: %.1f | V: %.1f | Satellites in View: %d", gps.getSpeedOverGround(), gps.getCourseOverGround(), gps.getVariation(), gps.getSatellitesInView().size() );
+      System.out.format( "SOG: %.1f | COG: %.1f | V: %.1f | Satellites in View: %d | PRNs: %d\n",
+        gps.getSpeedOverGround(), gps.getCourseOverGround(), gps.getVariation(), gps.getSatellitesInView().size(), gps.getPRNs().size() );
       System.out.println();
     }
   }
