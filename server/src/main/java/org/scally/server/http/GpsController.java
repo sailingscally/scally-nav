16e5366ce0e5ed2d1878c44b88c13f13a5644c81
Luis Martins <luis.martins@hitachivantara.com>
2020-04-24 15:04:09 +0100

    x

--- server/src/main/java/org/scally/server/http/GpsController.java
+++ server/src/main/java/org/scally/server/http/GpsController.java
@@ -1,6 +1,9 @@
 package org.scally.server.http;
 
+import org.scally.server.core.gps.GpsAntenna;
+import org.scally.server.core.gps.GpsFix;
 import org.scally.server.http.messages.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +13,25 @@ import java.util.concurrent.atomic.AtomicLong;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
-public class HelloController {
+@RequestMapping( "/api/gps" )
+public class GpsController {
+
+  private GpsAntenna gps;
 
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
-  @RequestMapping( method = GET, path = "/hello" )
-  public String greeting() {
-    return "Hello!";
+  @GetMapping( path = "/status" )
+  public GpsFix status() {
+    return gps != null ? gps.getFixStatus() : null;
   }
 
   @RequestMapping( method = GET, path = "/greeting" )
   public Greeting greeting( @RequestParam( value = "name", defaultValue = "World" ) String name ) {
     return new Greeting( counter.incrementAndGet(), String.format( template, name ) );
+  }
+
+  public void setGpsAntenna( GpsAntenna gps ) {
+    this.gps = gps;
   }
 }
