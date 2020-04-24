521e816fb10d5ba49f51ccc62e673b448363ce84
Luis Martins <luis.martins@hitachivantara.com>
2020-04-24 15:34:38 +0100

    x

--- core/src/main/java/org/scally/server/core/gps/GpsFix.java
+++ core/src/main/java/org/scally/server/core/gps/GpsFix.java
@@ -1,9 +1,14 @@
 package org.scally.server.core.gps;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum GpsFix {
 
+  @JsonProperty( "none" )
   NONE,
+  @JsonProperty( "2D" )
   FIX_2D,
+  @JsonProperty( "3D" )
   FIX_3D;
 
 }
